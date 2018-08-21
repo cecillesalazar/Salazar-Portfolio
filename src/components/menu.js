e1d9c7300cf6c67c9a5774cf409b1a1841e326aa
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-21 10:22:43 -0700

    Added logo placeholder and to portfolio button - not wired

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import '../components-css/menu.css';
 
 export default class Menu extends React.Component {
   render() {
-    return <p>Hello world</p>
+    return (
+      <div className="menu-body">
+        <img className="logo" src="https://s25.postimg.cc/tmobd6ypb/Logo_Makr_8by_J87.png" alt="portfolio logo"/>
+        <div className="round">
+          <a class="button" href="#">Open</a>
+        </div>
+      </div>
+    )
   }
 }
