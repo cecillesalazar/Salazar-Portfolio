3d5f144c63a2a91909ca93e310797b8e7f7b3bcf
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-21 15:20:53 -0700

    Added store to Provider tags in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import Menu from '../src/components/menu';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Menu />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Menu />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
