949448ccd1e7a608afb6143e0ec42398bbfde312
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-28 16:37:02 -0700

    Created floating animation

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import '../components-css/dashboard.css';
+import TypeWriter from 'react-typewriter'
 
 export default class Dashboard extends React.Component {
   render() {
-    return <p>Hello World</p>
+    return (
+      <div>
+          <h1>
+            <TypeWriter typing={1}>
+            Full Stack Developer
+            <br/>Designer Newbie
+            <br/>Shiba Inu Mom :)
+            </TypeWriter>
+          </h1>
+
+          <div className="floating">
+          </div>
+      </div>
+    )
 
 
-      //For After Sketches Submitted
-      // <div>
-      //   <p className="character-name">Cecille</p>
-      //   <div className="character">
-      //     <p className="character-box-text">Character Bust Placeholder</p>
-      //   </div>
-      //   <div className="dialogue-box"></div>
-      // </div>
+
 
   }
 }
+
+//For After Sketches Submitted
+// <div>
+//   <p className="character-name">Cecille</p>
+//   <div className="character">
+//     <p className="character-box-text">Character Bust Placeholder</p>
+//   </div>
+//   <div className="dialogue-box"></div>
+// </div>
