d4632fddc3975da0d4f1b145c41033c2bc287ace
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-29 06:45:29 -0700

    Added sidebar component to be implemented

---
+++ src/components/sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <div className="orange"></div>
+        <div className="yellow"></div>
+        <div className="blue"></div>
+        <div className="green"></div>
+        <div className="red"></div>
+      </div>
+    )
+  }
+}
