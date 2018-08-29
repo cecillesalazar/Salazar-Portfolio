0e21332be8eae3a6b6113646755f7d1deeb2fc78
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-29 16:00:52 -0700

    Conditionally rendered the floats based on the clicking of the first down button

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -6,32 +6,57 @@ import Stats from './stats';
 import TypeWriter from 'react-typewriter'
 
 export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstDownButton: false
+    }
+  }
+
+  firstButtonClicked() {
+    this.setState({
+      firstDownButton: true
+    })
+  }
+
   render() {
-    return (
-      <div className="dashboard">
-        <Sidebar />
-        <div className="tagline-container">
-          <h1 className="tagline">
-            <TypeWriter typing={1}>
-            Full Stack Developer
-            <br/>Designer Newbie
-            <br/>Shiba Inu Mom :)
-            </TypeWriter>
-          </h1>
+    if(this.state.firstDownButton) {
+      return (
+        <div className="dashboard">
+          <Sidebar />
+          <div className="tagline-container">
+            <h1 className="tagline">
+              <TypeWriter typing={1}>
+              Full Stack Developer
+              <br/>Designer Newbie
+              <br/>Shiba Inu Mom :)
+              </TypeWriter>
+            </h1>
+          </div>
+          <Floats />
+          <Stats />
         </div>
-        <Floats />
-        <div className="down-button">
-          <a href="#">
+      )
+    } else {
+      return (
+        <div className="dashboard">
+          <Sidebar />
+          <div className="tagline-container">
+            <h1 className="tagline">
+              <TypeWriter typing={1}>
+              Full Stack Developer
+              <br/>Designer Newbie
+              <br/>Shiba Inu Mom :)
+              </TypeWriter>
+            </h1>
+          </div>
+          <a href="#" onClick={() => this.firstButtonClicked()}>
             <span class="bottom"></span>
           </a>
-        <Stats />
         </div>
-      </div>
-    )
-
-
-
-
+      )
+    }
   }
 }
 
