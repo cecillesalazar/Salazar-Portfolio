aec7968c3e42dc0980656a60d69ade365f6d89d8
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-28 18:44:36 -0700

    Housed the tagline in a div for organization

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -7,19 +7,21 @@ export default class Dashboard extends React.Component {
   render() {
     return (
       <div>
-          <h1>
+        <div className="tagline-container">
+          <h1 className="tagline">
             <TypeWriter typing={1}>
             Full Stack Developer
             <br/>Designer Newbie
             <br/>Shiba Inu Mom :)
             </TypeWriter>
           </h1>
-          <Floats />
-          <div className="down-button">
-            <a href="#">
-              <span class="bottom"></span>
-            </a>
-          </div>
+        </div>
+        <Floats />
+        <div className="down-button">
+          <a href="#">
+            <span class="bottom"></span>
+          </a>
+        </div>
       </div>
     )
 
