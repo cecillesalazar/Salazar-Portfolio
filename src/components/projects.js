55c38b5f80542a0a084583fced8527a5b577ff10
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-01 15:37:57 -0700

    Added some line breaks at the very bottom of project.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,9 +5,13 @@ export default class Projects extends React.Component {
   render() {
     return (
       <div className="">
-        <h1>Current Projects...</h1>
+        <h1>{'What I\'m working on...'}</h1>
         <div className="Historia"></div>
         <div className="Miko-Adventures"></div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
       </div>
     )
   }
