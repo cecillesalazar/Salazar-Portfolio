88e7f58aa7ce5e90aefc27b5edead69bfd503242
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 16:08:48 -0700

    Added more whitespace in contact component

--- src/components/contact.js
+++ src/components/contact.js
@@ -16,6 +16,10 @@ export default class Contact extends React.Component {
         <br/>
         <br/>
         <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <h2 className="closing-quote">"Work and play are words used to describe the same thing under differing conditions."</h2>
         <h4 className="quote-author">Mark Twain</h4>
         <br/>
