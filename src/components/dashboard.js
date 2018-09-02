fedc513dd7350c165715bed93b98ff11ffce6f1a
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 15:16:40 -0700

    Cleaned up dashboard component and changed tagline text

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,13 +3,12 @@ import '../components-css/dashboard.css';
 import Floats from './floats';
 import Sidebar from './sidebar';
 import SocialMedia from './socialmedia';
-import TypeWriter from 'react-typewriter'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import TypeWriter from 'react-typewriter';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props)
-
   }
 
   render() {
@@ -22,8 +21,8 @@ export default class Dashboard extends React.Component {
               <h1 className="tagline">
                 <TypeWriter typing={1}>
                 Full Stack Developer
-                <br/>Designer Newbie
-                <br/>Shiba Inu Mom :)
+                <br/>Design Enthusiast
+                <br/>Proud Shiba mom :)
                 </TypeWriter>
               </h1>
               <Route exact path="/about" component={Floats} />
