62d3e30f8df5339db37282e3dcfa9d1b4787bb44
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 14:47:19 -0700

    Added text for pecentages into projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -20,38 +20,38 @@ export default class Projects extends React.Component {
           <div className="historia-stats-container">
             <p className="labels">HTML5</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px" }}>
-              <div className="bar" style={{ height: "24px", width: "10%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "10%", backgroundColor: "#c4d70c" }}>10%</div>
             </div>
             <p className="labels" style={{ top: "12%" }}>Node</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "12%" }}>
-              <div className="bar" style={{ height: "24px", width: "10%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "10%", backgroundColor: "#c4d70c" }}>10%</div>
             </div>
             <p className="labels" style={{ top: "24%" }}>React</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "24%" }}>
-              <div className="bar" style={{ height: "24px", width: "40%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "40%", backgroundColor: "#c4d70c" }}>40%</div>
             </div>
             <p className="labels" style={{ top: "36%" }}>Redux</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "36%" }}>
-              <div className="bar" style={{ height: "24px", width: "30%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "30%", backgroundColor: "#c4d70c" }}>30%</div>
             </div>
             <p className="labels" style={{ top: "48%" }}>Bcrypt</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "48%" }}>
-              <div className="bar" style={{ height: "24px", width: "5%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "5%", backgroundColor: "#c4d70c" }}>5%</div>
             </div>
             <p className="labels" style={{ top: "60%" }}>JWT</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "60%" }}>
-              <div className="bar" style={{ height: "24px", width: "5%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "5%", backgroundColor: "#c4d70c" }}>5%</div>
             </div>
           </div>
 
           <div className="miko-adventures-stats-container">
             <p className="labels" style={{ top: "12%" }}>Phaser</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", backgroundColor: "#ffe52c" }}>
-              <div className="bar" style={{ height: "24px", width: "80%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "80%", backgroundColor: "#c4d70c" }}>80%</div>
             </div>
             <p className="labels">HTML5</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", backgroundColor: "#ffe52c", top: "12%" }}>
-              <div className="bar" style={{ height: "24px", width: "20%", backgroundColor: "#c4d70c" }}></div>
+              <div className="bar" style={{ height: "24px", width: "20%", backgroundColor: "#c4d70c" }}>20%</div>
             </div>
           </div>
 
@@ -60,7 +60,7 @@ export default class Projects extends React.Component {
               <span className="bottom"></span>
             </Link>
           </a>
-          
+
           <Route exact path="/contact" component={Contact} />
         </div>
       </Router>
