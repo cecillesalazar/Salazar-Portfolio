2beaec6794388aee688b68c7265384f51284236d
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 09:06:11 -0700

    Created contact component

---
+++ src/components/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../components-css/contact.css';
+
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Interested in collaborating?</h1>
+        <p>{'Let\'s grab coffee (on me)!'}</p>
+        <ul>
+          <li>Slack: cecillesalazar</li>
+          <li>Email: cecillersalazar@gmail.com</li>
+        </ul>
+      </div>
+    )
+  }
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import '../components-css/projects.css';
+import Contact from './contact';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class Projects extends React.Component {
   render() {
     return (
-      <div className="">
-        <h1>{'What I\'m working on...'}</h1>
-        <div className="Historia">
-          <p className="historia-text">Historia<br/><br/>A full stack create-your-own-<br/>adventure visual novel app</p>
+      <Router>
+        <div className="">
+          <h1>{'What I\'m working on...'}</h1>
+          <div className="Historia">
+            <p className="historia-text">Historia<br/><br/>A full stack create-your-own-<br/>adventure visual novel app</p>
+          </div>
+          <div className="Miko-Adventures">
+            <p className="miko-adventures-text">{'Miko\'s Adventure'}<br/><br/> A pixel art game of<br/>my Shiba inu’s daily adventures</p>
+          </div>
+          <a className="down-button-3" href="#">
+            <Link to="/contact">
+              <span className="bottom"></span>
+            </Link>
+          </a>
+          <Route exact path="/contact" component={Contact} />
         </div>
-        <div className="Miko-Adventures">
-          <p className="miko-adventures-text">{'Miko\'s Adventure'}<br/><br/> A pixel art game of<br/>my Shiba inu’s daily adventures</p>
-        </div>
-      </div>
+      </Router>
     )
   }
 }
