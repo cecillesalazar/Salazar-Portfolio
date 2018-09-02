4acf628c3b8598956d9d8d58b7bf81982dddd930
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 10:34:19 -0700

    Created containers for stats

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,9 +15,10 @@ export default class Projects extends React.Component {
           <div className="Miko-Adventures">
             <p className="miko-adventures-text">{'Miko\'s Adventure'}<br/><br/> A pixel art game of<br/>my Shiba inu’s daily adventures</p>
           </div>
-          <div className="border" style={{ border: "2px solid black", width: "300px" }}>
-            <div className="bar" style={{ height: "24px", width: "20%", backgroundColor: "gray" }}></div>
-          </div>
+          <br/>
+          <br/>
+          <div className="historia-stats-container"></div>
+          <div className="miko-adventures-stats-container"></div>
           <a className="down-button-3" href="#">
             <Link to="/contact">
               <span className="bottom"></span>
