ef3b5b161bf5be901798a3204a33b8b63e5694aa
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 21:26:26 -0700

    Swapped elements in projects in order to fit smaller viewports for future media queries

--- src/components/projects.js
+++ src/components/projects.js
@@ -13,38 +13,35 @@ export default class Projects extends React.Component {
           <div className="Historia">
             <p className="historia-text">Historia<br/><br/>A full stack create-your-own-<br/>adventure visual novel app</p>
           </div>
-          <div className="Miko-Adventures">
-            <p className="miko-adventures-text">{'Miko\'s Adventure'}<br/><br/> A pixel art game of<br/>my Shiba inu’s daily adventures</p>
-          </div>
-          <br/>
-          <br/>
           <div className="historia-stats-container">
             <p className="labels">HTML5</p>
-            <div className="border" style={{ border: "2px solid #c22303", width: "300px" }}>
+            <div className="border" style={{ border: "2px solid #c22303", width: "300px", backgroundColor: "#ffe52c" }}>
               <div className="bar" style={{ height: "24px", width: "10%", backgroundColor: "#c4d70c" }}>10%</div>
             </div>
             <p className="labels" style={{ top: "12%" }}>Node</p>
-            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "12%" }}>
+            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "12%", backgroundColor: "#ffe52c" }}>
               <div className="bar" style={{ height: "24px", width: "10%", backgroundColor: "#c4d70c" }}>10%</div>
             </div>
             <p className="labels" style={{ top: "24%" }}>React</p>
-            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "24%" }}>
+            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "24%", backgroundColor: "#ffe52c" }}>
               <div className="bar" style={{ height: "24px", width: "40%", backgroundColor: "#c4d70c" }}>40%</div>
             </div>
             <p className="labels" style={{ top: "36%" }}>Redux</p>
-            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "36%" }}>
+            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "36%", backgroundColor: "#ffe52c" }}>
               <div className="bar" style={{ height: "24px", width: "30%", backgroundColor: "#c4d70c" }}>30%</div>
             </div>
             <p className="labels" style={{ top: "48%" }}>Bcrypt</p>
-            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "48%" }}>
+            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "48%", backgroundColor: "#ffe52c" }}>
               <div className="bar" style={{ height: "24px", width: "5%", backgroundColor: "#c4d70c" }}>5%</div>
             </div>
             <p className="labels" style={{ top: "60%" }}>JWT</p>
-            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "60%" }}>
+            <div className="border" style={{ border: "2px solid #c22303", width: "300px", top: "60%", backgroundColor: "#ffe52c" }}>
               <div className="bar" style={{ height: "24px", width: "5%", backgroundColor: "#c4d70c" }}>5%</div>
             </div>
           </div>
-
+          <div className="Miko-Adventures">
+            <p className="miko-adventures-text">{'Miko\'s Adventure'}<br/><br/> A pixel art game of<br/>my Shiba inu’s daily adventures</p>
+          </div>
           <div className="miko-adventures-stats-container">
             <p className="labels" style={{ top: "12%" }}>Phaser</p>
             <div className="border" style={{ border: "2px solid #c22303", width: "300px", backgroundColor: "#ffe52c" }}>
