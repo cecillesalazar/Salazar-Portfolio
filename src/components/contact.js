2e010c913fb39e392aa6032bc6297ccea46940d3
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-02 18:21:04 -0700

    Used typewriter effect on all text in contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,26 +6,28 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div>
-      <h1><TypeWriter typing={1}>Interested in collaborating?</TypeWriter></h1>
-        <h2 className="contact-header">{'Let\'s grab coffee (on me)!'}</h2>
-        <ul>
-          <li className="contact-li">Slack: cecillesalazar</li>
-          <li className="contact-li">Email: cecillersalazar@gmail.com</li>
-        </ul>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <h2 className="closing-quote">"Work and play are words used to describe the same thing under differing conditions."</h2>
-        <h4 className="quote-author">Mark Twain</h4>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+      <TypeWriter typing={1}>
+        <h1>Interested in collaborating?</h1>
+          <h2 className="contact-header">{'Let\'s grab coffee (on me)!'}</h2>
+          <ul>
+            <li className="contact-li">Slack: cecillesalazar</li>
+            <li className="contact-li">Email: cecillersalazar@gmail.com</li>
+          </ul>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <h2 className="closing-quote">"Work and play are words used to describe the same thing under differing conditions."</h2>
+          <h4 className="quote-author">Mark Twain</h4>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+        </TypeWriter>
       </div>
     )
   }
