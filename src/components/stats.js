67a09f2760bdd76dd0a85b94943a5ae400a621da
Cecille Salazar <cecillersalazar@gmail.com>
2018-10-24 10:02:02 -0700

    High Level Basics - Responsiveness and Mobile first design

--- src/components/stats.js
+++ src/components/stats.js
@@ -16,8 +16,7 @@ export default class Stats extends React.Component {
         </section>
 
         <section className="hex-honeycomb">
-          <ul id="categories" class="clr">
-            <li class="pusher"></li>
+          <ul id="categories" className="clr">
           	<li>
                 <div className="node-logo">
                   <img src="https://s25.postimg.cc/4sz6p6533/Screen_Shot_2018-09-01_at_9.43.18_PM.png" alt=""/>
@@ -32,6 +31,7 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Advanced</p>
               </div>
             </li>
+            <li className="pusher"></li>
             <li>
                 <div className="chai-logo">
                   <img src="https://s25.postimg.cc/qdznynewf/Screen_Shot_2018-09-01_at_1.36.04_PM.png" alt=""/>
@@ -46,7 +46,7 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Intermediate</p>
               </div>
             </li>
-            <li class="pusher"></li>
+            <li className="pusher"></li>
             <li>
                 <div className="mongo-logo">
                   <img src="https://s25.postimg.cc/4g7sj09yn/Screen_Shot_2018-09-01_at_9.50.17_PM.png" alt=""/>
@@ -62,7 +62,7 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Intermediate</p>
               </div>
             </li>
-            <li class="pusher"></li>
+            <li className="pusher"></li>
             <li>
                 <div className="react-logo">
                   <img src="https://s25.postimg.cc/joxpwuvy7/Screen_Shot_2018-09-01_at_9.59.27_PM.png" alt=""/>
@@ -76,7 +76,9 @@ export default class Stats extends React.Component {
                   <h1>Redux</h1>
                   <p>Proficiency Level: Advanced</p>
               </div>
-            </li><li>
+            </li>
+            <li className="pusher"></li>
+            <li>
                 <div className="javascript-logo">
                   <img src="https://s25.postimg.cc/db8mtobnj/Screen_Shot_2018-09-01_at_10.07.54_PM.png" alt=""/>
                   <h1>Javascript</h1>
@@ -90,6 +92,7 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Advanced</p>
               </div>
             </li>
+            <li className="pusher"></li>
             <li>
                 <div className="jquery-logo">
                   <img src="https://s25.postimg.cc/nzcdry8lr/Screen_Shot_2018-09-01_at_10.12.33_PM.png" alt=""/>
@@ -97,8 +100,6 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Beginner</p>
               </div>
             </li>
-            <li class="pusher"></li>
-            <li class="pusher"></li>
             <li>
                 <div className="sass-logo">
                   <img src="https://s25.postimg.cc/6m23d65lr/Screen_Shot_2018-09-01_at_10.22.14_PM.png" alt=""/>
@@ -106,7 +107,7 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Beginner</p>
               </div>
             </li>
-            <li class="pusher"></li>
+            <li className="pusher"></li>
             <li>
                 <div>
                   <img src="https://i.postimg.cc/m2Lmz9q2/Screen_Shot_2018-09-26_at_7.27.39_PM.png" alt=""/>
@@ -121,8 +122,7 @@ export default class Stats extends React.Component {
                   <p>Proficiency Level: Beginner</p>
               </div>
             </li>
-            <li class="pusher"></li>
-            <li class="pusher"></li>
+            <li className="pusher"></li>
             	<li>
                 <div>
                   <img src="https://i.postimg.cc/FKknNCpF/Screen-Shot-2018-10-14-at-5-21-00-PM.png" alt=""/>
